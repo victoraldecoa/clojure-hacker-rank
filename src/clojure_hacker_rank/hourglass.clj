6d68e09fc990938e97ead998ee79126806eaee09
Victor Aldecoa <victor.maia.a@gmail.com>
2020-01-04 19:40:15 -0300

    Refactor hourglass to use threading macros

--- src/clojure_hacker_rank/hourglass.clj
+++ src/clojure_hacker_rank/hourglass.clj
@@ -3,19 +3,17 @@
 (defn hourglassSum [arr]
   (let
    [minValue Integer/MIN_VALUE]
-    (reduce
-     max
-     (apply
-      concat
-      (map-indexed
-       (fn [i line]
-         (map-indexed
-          (fn [j elem]
-            (+
-             (nth (nth arr (dec i) []) (dec j) minValue)
-             (nth (nth arr (dec i) []) j minValue)
-             (nth (nth arr (dec i) []) (inc j) minValue)
-             elem
-             (nth (nth arr (inc i) []) (dec j) minValue)
-             (nth (nth arr (inc i) []) j minValue)
-             (nth (nth arr (inc i) []) (inc j) minValue))) line)) arr)))))
+    (->> (map-indexed
+          (fn [i line]
+            (map-indexed
+             (fn [j elem]
+               (+
+                (nth (nth arr (dec i) []) (dec j) minValue)
+                (nth (nth arr (dec i) []) j minValue)
+                (nth (nth arr (dec i) []) (inc j) minValue)
+                elem
+                (nth (nth arr (inc i) []) (dec j) minValue)
+                (nth (nth arr (inc i) []) j minValue)
+                (nth (nth arr (inc i) []) (inc j) minValue))) line)) arr)
+         (apply concat)
+         (reduce max))))
