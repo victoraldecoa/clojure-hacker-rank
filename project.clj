4f6febacc579f43432c7b0a3adda5a04d4a22b59
Victor Aldecoa <victor.maia.a@gmail.com>
2020-10-24 15:07:31 -0300

    Use project options to report codecov format and exclude core namespace

--- project.clj
+++ project.clj
@@ -4,6 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :plugins [[lein-cloverage "1.1.2"]]
+  :cloverage {:ns-exclude-regex [#"clojure-hacker-rank.core"]
+              :codecov? true}
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :main ^:skip-aot clojure-hacker-rank.core
   :target-path "target/%s"
