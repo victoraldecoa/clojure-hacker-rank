e941392053e2c1a528ae67d5691ae4169bc991c5
Victor Aldecoa <victor.maia.a@gmail.com>
2020-10-24 13:47:18 -0300

    Remove legacy rotate-left implementation

--- src/clojure_hacker_rank/rotate_left.clj
+++ src/clojure_hacker_rank/rotate_left.clj
@@ -2,11 +2,3 @@
 
 (defn rotate-left [a d]
   (concat (subvec a d) (subvec a 0 d)))
-
-(defn slow-rotate-left [a d]
-  (loop [i d
-         b a]
-    (println "looping " i " array " b)
-    (if (= i 0)
-      b
-      (recur (dec i) (drop 1 (conj (vec b) (first b)))))))
