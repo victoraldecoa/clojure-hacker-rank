20375aafe59b3b923b178c5e113d10e3858ec452
Victor Aldecoa <victor.maia.a@gmail.com>
2020-10-24 13:26:31 -0300

    Include Codecov coverage tool

--- project.clj
+++ project.clj
@@ -3,6 +3,7 @@
   :url "https://github.com/victoraldecoa/clojure-hacker-rank"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
+  :plugins [[lein-cloverage "1.1.2"]]
   :dependencies [[org.clojure/clojure "1.10.0"]]
   :main ^:skip-aot clojure-hacker-rank.core
   :target-path "target/%s"
